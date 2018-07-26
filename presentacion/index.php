13b020e96a5447201bdf3f8dd66481b6e80008a2
Unknown <max.j.s.kaechele@gmail.com>
2018-07-26 02:37:30 -0400

    barra lateral

--- presentacion/index.php
+++ presentacion/index.php
@@ -4,10 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/styles.css">
     <title>feria de musica</title>
 </head>
 <body>
+    <!-- contiene el menu lateral(horizontal)
+        INICIO -->
+    <div class="sidebar">
+        <ul>
+            <h1>menu</h1>
+            <li><a href="">inicio</a></li>
+            <li><a href="">vision y mision</a></li>
+            <li><a href="">catalogo</a></li>
+        </ul>
+    </div>
+    <!-- FIN -->
     <div class="contenedor">
+        <img class="menu_bar"src="assets/img/menu-button.png" alt="">
 
     </div>
 </body>
